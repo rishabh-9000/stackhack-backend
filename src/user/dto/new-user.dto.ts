9bfb859d312bfaf0902e1a36f6b725335967a68a
Rishabh Dubey <rishabhdubey020@gmail.com>
2020-05-11 03:11:43 +0530

    added validation

--- src/user/dto/new-user.dto.ts
+++ src/user/dto/new-user.dto.ts
@@ -1,7 +1,34 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  Max,
+  IsInt,
+  MaxLength,
+  IsIn,
+  IsAlpha,
+} from 'class-validator';
+
 export class NewUserDto {
+  @IsNotEmpty()
+  @MaxLength(100)
+  @IsAlpha()
   readonly fullName: string;
+
+  @IsNotEmpty()
+  @MaxLength(10)
   readonly mobile: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  @MaxLength(100)
   readonly email: string;
+
+  @IsNotEmpty()
+  @IsIn(['self', 'group', 'corporate', 'others'])
   readonly registrationType: string;
+
+  @IsNotEmpty()
+  @IsInt()
+  @Max(10)
   readonly numberOfTickets: number;
 }
